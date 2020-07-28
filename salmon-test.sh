60d636cc1407ecc9461ef8cddcad4d8b0113b927
David J. H. Shih <djh.shih@gmail.com>
2020-07-28 16:04:37 -0500

    Removed obsolete file.

--- salmon-test.sh
+++
@@ -1,3 +0,0 @@
-#salmon quant -i ~/data/gencode/release-28/salmon_quasi -l ISR -1 cutadapt2/fastq/10-C20-V-1_S67_L008_R1_001.cut2.fastq.gz -2 cutadapt2/fastq/10-C20-V-1_S67_L008_R2_001.cut2.fastq.gz -o salmon/cutadapt2/10-C20-V-1
-
-salmon quant -i ~/data/gencode/release-28/salmon_quasi -l ISR -1 fastq/10-C20-V-1_S67_L008_R1_001.fastq.gz -2 fastq/10-C20-V-1_S67_L008_R2_001.fastq.gz -o salmon/10-C20-V-1
