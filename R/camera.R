ac5db699ba9c8ebd96ee1e47c90451f5b2f9b3aa
David J. H. Shih <djh.shih@gmail.com>
2020-08-31 21:11:59 -0500

    Improved plotting for cmap analysis.

--- R/camera.R
+++ R/camera.R
@@ -1,4 +1,6 @@
 library(limma)
+library(ggplot2)
+library(ggrepel)
 
 camera_single <- function(s, gsets=NULL, index=NULL) {
 	if (is.null(index)) {
@@ -61,33 +63,55 @@ camera_transform <- function(y, gsets) {
 	res
 }
 
-cam_volcano_plot <- function(cam, fdr.cut=0.01, z.cut=0.5, label.size=4, rename_gset=identity) {
+cam_volcano_plot <- function(cam, fdr.cut=0.01, z.cut=0.5, nudge=0.1, label.size=4, down.sample=NA, rename_gset=identity, show.legend=FALSE, two.sided=TRUE, ylim=NULL) {
 	if (is.null(cam$gset)) {
 		cam$gset = rownames(cam);
 	}
 
 	cam <- mutate(cam,
-		keep = FDR < fdr.cut & abs(z1) > z.cut,
-		group = factor(
-			ifelse(keep, ifelse(delta < 0, "Down", "Up"), "NS"),
-			c("Down", "Up", "NS")
-		),
 		gset = rename_gset(gset)
 	);
 
-	ggplot(cam,
+	if (is.null(cam$keep)) {
+		cam <- mutate(cam, keep = FDR < fdr.cut & abs(z1) > z.cut);
+	}
+
+	if (is.null(cam$group)) {
+		cam <- mutate(cam,
+			group = factor(
+				ifelse(keep, ifelse(z1 < 0, "Down", "Up"), "NS"),
+				c("Down", "Up", "NS")
+			)
+		);
+	}
+
+	if (!is.na(down.sample)) {
+		# hide a set of insigificant data points
+		idx <- which(cam$FDR >= fdr.cut);
+		# higher FDR => higher chance of being omitted
+		omit <- sample(idx, floor(down.sample * length(idx)), prob = cam$FDR[idx]);
+		cam$FDR[omit] <- NA;
+	}
+
+	g <- ggplot(cam,
 		aes(
 			x=z1, y=FDR, colour=group,
 			alpha = ifelse(keep, 0.7, 0.2),
 			label = ifelse(keep, gset, NA)
 		)
-	) + theme_clean() +
-		geom_vline(xintercept=0) +
-		geom_vline(xintercept=c(z.cut, -z.cut), linetype=3, colour="grey60") +
+	) + theme_clean();
+
+	if (two.sided) {
+		g <- g + geom_vline(xintercept=0) + 
+			scale_y_continuous(trans=revlog_trans(10), limits=ylim, sec.axis = dup_axis(name=NULL));
+	} else {
+		g <- g + scale_y_continuous(trans=revlog_trans(10), limits=ylim);
+	}
+
+	g <- g + geom_vline(xintercept=c(z.cut, -z.cut), linetype=3, colour="grey60") +
 		geom_hline(yintercept=fdr.cut, linetype=3, colour="grey60") +
-		geom_point(show.legend=FALSE) +
-		geom_label_repel(show.legend=FALSE, nudge_y=0.1, nudge_x=0.1, size=label.size) +
-		scale_y_continuous(trans=revlog_trans(10), sec.axis = dup_axis(name=NULL)) +
+		geom_point(show.legend=show.legend) +
+		geom_label_repel(show.legend=FALSE, nudge_y=nudge, nudge_x=nudge, size=label.size) +
 		scale_colour_manual(values=diff.group.cols) +
 		xlab("mean expression difference") + ylab("false discovery rate")
 }
--- cmap/analyze-similarity.R
+++ cmap/analyze-similarity.R
@@ -5,10 +5,11 @@ library(limma)
 library(ComplexHeatmap)
 library(ggplot2)
 library(reshape2)
-#library(viridis)
 library(RColorBrewer)
 library(io)
 
+source("../R/preamble.R")
+source("../R/camera.R")
 
 lincs.indir <- "~/data/cmap/phase1";
 
@@ -20,6 +21,8 @@ pheno <- qread(file.path(lincs.indir, "lincs-sig-info.rds"));
 cell <- qread(file.path(lincs.indir, "lincs-cell-info.rds"));
 fannot <- qread(file.path(lincs.indir, "lincs-gene-info_lm.rds"));
 groups <- qread(file.path(lincs.indir, "lincs-sig-groups.rds"));
+
+compound.annot <- qread("compound-annot.tsv");
 
 #in.fname <- as.filename("../salmon/parpi-resist_deseq-stat_treatment-clone-interaction_clones-vs-parental.mtx");
 in.fname <- as.filename("../salmon/parpi-resist_deseq-stat_treatment-clone-interaction_treatment_clones-vs-parental.mtx");
@@ -35,6 +38,13 @@ x <- qread(in.fname);
 
 ss <- qread(ss.fname);
 ss.null <- qread(ss.null.fname);
+
+group_name_to_clone <- function(x) {
+	as.character(factor(sub(".*clone", "", x), levels=clones.from, labels=clones))
+}
+
+rownames(ss) <- group_name_to_clone(rownames(ss));
+rownames(ss.null) <- group_name_to_clone(rownames(ss.null));
 
 out.fname <- fname;
 
@@ -58,11 +68,10 @@ table(is.na(x.idx))
 # NB x is populated with NAs in order to match the reference data
 x <- x[x.idx, ];
 
+colnames(x) <- group_name_to_clone(colnames(x));
+
 
 ##############################################################################
-
-clones <- sub(".*clone(C.*)", "\\1", rownames(ss));
-rownames(ss) <- clones;
 
 mean_transform <- function(ss, group) {
 	res <- apply(ss, 1,
@@ -126,7 +135,7 @@ median_percentile_transform <- function(ss, group) {
 	res
 }
 
-camera_transform <- function(ss, group) {
+camera_fdr_transform <- function(ss, group) {
 	res <- apply(ss, 1,
 		function(ss1) {
 			d <- cameraPR(ss1, group, sort=FALSE);
@@ -144,12 +153,69 @@ camera_transform <- function(ss, group) {
 	res
 }
 
+test_vs_null <- function(ss1, ss.null1, alternative = c("two.sided", "less", "greater")) {
+	mu <- mean(ss.null1);
+	sigma <- sd(ss.null1);
+
+	alternative <- match.arg(alternative);
+
+	if (alternative == "less") {
+		pnorm(ss1, mean=mu, sd=sigma, lower.tail=TRUE)
+	} else if (alternative == "greater") {
+		pnorm(ss1, mean=mu, sd=sigma, lower.tail=FALSE)
+	} else {
+		pmin(1, pnorm(-abs(ss1), mean=mu, sd=sigma, lower.tail=TRUE) * 2)
+	}
+}
+
+####
+
+qdraw(
+	{
+		plot(density(ss.null), las=1, xlab="Cosine similarity", main=NA, col="grey30", lwd=2, bty="n")
+		lines(density(ss), las=1, xlab="Cosine similarity", main=NA, col="firebrick", lwd=2)
+	},
+	width = 4, height = 3,
+	file = tag(out.fname, c("cos-sim", "density"), ext="pdf")
+);
+
+####
+
+ss.p <- do.call(rbind, lapply(
+	rownames(ss),
+	function(i) {
+		test_vs_null(ss[i, ], ss.null[i, ], alternative="greater")
+	}
+));
+rownames(ss.p) <- rownames(ss);
+colnames(ss.p) <- colnames(ss);
+
+hist(ss.p, breaks=100)
+
+ss.q <- t(apply(ss.p, 1, p.adjust, method="BH"));
+
+hist(ss.q, breaks=100)
+
+ss.q.min <- min_transform(ss.q, groups$trt_cp);
+
+ss.lq <- log10(ss.q.min);
+
+idx <- apply(ss.lq, 1, function(lq) any(lq < log10(0.05)));
+
+ss.lq[idx, ]
+
+idx <- order(apply(ss.lq, 1, mean));
+
+ss.lq[idx[1:100], ]
+
+####
+
 ms.cp <- mean_transform(ss, groups$trt_cp);
 mx.cp <- max_transform(ss, groups$trt_cp);
 mn.cp <- min_transform(ss, groups$trt_cp);
 mp.cp <- median_percentile_transform(ss, groups$trt_cp);
 
-es.cp <- camera_transform(ss, groups$trt_cp);
+es.cp <- camera_fdr_transform(ss, groups$trt_cp);
 
 #idx <- apply(es.cp, 1, function(lq) any(lq > 6));
 idx <- apply(es.cp, 1, function(lq) any(lq > 10));
@@ -158,18 +224,18 @@ mp.cp.sub <- mp.cp[idx, , drop=FALSE];
 ms.cp.sub <- ms.cp[idx, , drop=FALSE];
 mx.cp.sub <- mx.cp[idx, , drop=FALSE];
 
-remap_samples <- function(samples) {
-	sample.id <- as.integer(sub("C", "", samples));
-	si <- order(sample.id);
-	factor(sample.id, levels=sample.id[si], label=paste0("RC", 1:length(sample.id)))
-}
+#remap_samples <- function(samples) {
+#	sample.id <- as.integer(sub("C", "", samples));
+#	si <- order(sample.id);
+#	factor(sample.id, levels=sample.id[si], label=paste0("RC", 1:length(sample.id)))
+#}
 
 simplify_features <- function(features) {
 	sub("^.+,", "", features)
 }
 
 simplify_names <- function(z) {
-	colnames(z) <- remap_samples(colnames(z));
+	#colnames(z) <- remap_samples(colnames(z));
 	rownames(z) <- simplify_features(rownames(z));
 	z
 }
@@ -185,22 +251,104 @@ Heatmap(simplify_names(mp.cp.sub), col=colf, heatmap_legend_param=list(title="pe
 dev.off();
 
 colf <- circlize::colorRamp2(c(-0.15, 0, 0.15), c(pal[1], pal[4], pal[7]));
-Heatmap(mx.cp.sub, col=colf)
+Heatmap(es.cp.sub, col=colf)
 
+####
+
+cams <- lapply(colnames(es.cp),
+	function(clone) {
+	data.frame(
+		gset = sub("trt_cp,", "", rownames(es.cp)),
+		z1 = ms.cp[, clone],
+		FDR = 10^(-abs(es.cp[, clone])),
+		stringsAsFactors=FALSE
+	)
+});
+names(cams) <- colnames(es.cp);
+
+
+
+for (clone in names(cams)) {
+	d <- cams[[clone]];
+	d.a <- left_join(d, rename(compound.annot, gset=pert_iname, group=compound_class));
+	d.a$group <- factor(d.a$group, levels=c("MEK inhibitor", "ERK inhibitor", "RAF inhibitor", "PI3K inhibitor"));
+
+	qdraw(
+		cam_volcano_plot(filter(d.a, z1 > 0), fdr.cut=1e-06, z.cut = 0.05, nudge=0, down.sample=0.97, show.legend=TRUE, two.sided=FALSE, ylim=c(1, 1e-30)) + 
+			guides(alpha=FALSE, colour="legend") + labs(colour="compound class") +
+			xlim(0, 0.15) +
+			xlab("mean cosine similarity") +
+			scale_colour_npg(na.value="grey40") +
+			ggtitle(clone)
+		,
+		width = 8, height = 5,
+		file = insert(out.fname, c("cmap-cosine", "camera", "volcano", clone), ext="pdf")
+	);
+}
+
+cam.all <- do.call(rbind, 
+	mapply(
+		function(d, clone) {
+			data.frame(d, clone=clone)
+		},
+		cams,
+		names(cams),
+		SIMPLIFY=FALSE
+	)
+);
+rownames(cam.all) <- NULL;
+cam.all <- rename(cam.all, clone = clone, compound = gset, similarity = z1, fdr = FDR);
+
+qwrite(cam.all, insert(out.fname, c("cmap-cosine", "camera"), ext="tsv"));
+
+####
+
+ss.m <- matrix(colMeans(ss), nrow=1);
+colnames(ss.m) <- colnames(ss);
+
+m.es.cp <- camera_fdr_transform(ss.m, groups$trt_cp);
+m.ms.cp <- mean_transform(ss.m, groups$trt_cp);
+
+m.cam <- data.frame(
+	gset = sub("trt_cp,", "", rownames(m.es.cp)),
+	z1 = m.ms.cp[, 1],
+	FDR = 10^(-abs(m.es.cp[, 1])),
+	stringsAsFactors=FALSE
+);
+rownames(m.cam) <- NULL;
+
+m.cam$group <- m.cam$gset %in% c("vinblastine", "vincristine");
 
 qdraw(
-	{
-		plot(density(ss.null), las=1, xlab="Cosine similarity", main=NA, col="grey30", lwd=2, bty="n")
-		lines(density(ss), las=1, xlab="Cosine similarity", main=NA, col="firebrick", lwd=2)
-	},
-	width = 4, height = 3,
-	file = tag(out.fname, c("cos-sim", "density"), ext="pdf")
+	cam_volcano_plot(m.cam, fdr.cut=1e-06, z.cut = 0.025, nudge=-0.05, down.sample=0.97, two.sided=FALSE) + 
+		xlim(-0.2, 0) +
+		xlab("mean cosine similarity") +
+		scale_color_manual(values=c("grey30", "#2166AC")) +
+		ggtitle("RCs")
+	,
+	width = 8, height = 5,
+	file = insert(out.fname, c("cmap-cosine", "camera", "volcano", "all", "down"), ext="pdf")
+);
+
+m.cam.a <- left_join(m.cam, rename(compound.annot, gset=pert_iname, group=compound_class));
+m.cam.a$group <- factor(m.cam.a$group, levels=c("MEK inhibitor", "ERK inhibitor", "RAF inhibitor", "PI3K inhibitor"));
+
+qdraw(
+	cam_volcano_plot(filter(m.cam.a, z1 > 0), fdr.cut=1e-06, z.cut = 0.03, nudge=0, down.sample=0.97, show.legend=TRUE, two.sided=FALSE, ylim=c(1, 1e-30)) + 
+		xlim(0, 0.075) +
+		guides(alpha=FALSE, colour="legend") + labs(colour="compound class") +
+		xlab("mean cosine similarity") +
+		scale_colour_npg(na.value="grey40") +
+		ggtitle("RCs")
+	,
+	width = 8, height = 5,
+	file = insert(out.fname, c("cmap-cosine", "camera", "volcano", "all"), ext="pdf")
 );
 
 ####
 
-idx <- apply(es.cp, 1, function(lq) any(lq < -6));
-#idx <- apply(es.cp, 1, function(lq) any(lq < -15));
+#idx <- apply(es.cp, 1, function(lq) any(lq < -6));
+idx <- apply(es.cp, 1, function(lq) any(lq < -15));
 es.cp.sub <- es.cp[idx, , drop=FALSE];
 mp.cp.sub <- mp.cp[idx, , drop=FALSE];
 ms.cp.sub <- mp.cp[idx, , drop=FALSE];
@@ -281,6 +429,7 @@ plot_compound("vinblastine");
 # AS-605240: PI3K inhibitor
 # PD-98059: MEK inhibitor
 # selumetinib: MEK inhibitor
+# trametinib: MEK inhibitor
 # ERK-inhibitor-11E: ERK inhibitor
 # PD-184352: MEK inhibitor
 # PD-0325901: MEK inhibitor
